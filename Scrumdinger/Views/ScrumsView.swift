31355f78c6f8ba776aa74f6c279fd9036a7ef8ab
Callum Inglis <callum.inglis@4oh4.co.uk>
2024-10-11 18:54:50 +0100

    Add Themes; Scrums View

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Callum Inglis on 11/10/2024.
+//
+
+import SwiftUI
+
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColour)
+        }
+    }
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
