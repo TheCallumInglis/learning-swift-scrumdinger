7bc3a3cf08e94014e28e2f1f9a1148c4c4e773d5
Callum Inglis <callum.inglis@4oh4.co.uk>
2024-10-12 20:11:07 +0100

    feat: Show ScrumList as Nav menu

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -12,9 +12,20 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.theme.mainColour)
+            }
+            .navigationTitle("Daily Scrum")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }
